33f1acc2ae557805bbf5163a1cd0cff699b8fa92
CrazyCaptian <dottage@gmail.com>
2021-12-16 18:09:47 -0600

    Update ForgeAuctions.sol

--- mainnetContracts/ForgeAuctions.sol
+++ mainnetContracts/ForgeAuctions.sol
@@ -1,7 +1,7 @@
 //  Forge Auctions Contract
 //   Auctions 8,192 tokens every 3 days and users are able to withdraw anytime after!
 
-//   All proceeds of auctions go back into the miners pockets, by going directly to the Proof of Work Contract!
+//   Proceeds of auctions go back into the miners pockets, by going directly to the Proof of Work Contract!
 
 // 10,500,000 tokens are Auctioned off over 100 years in this contract! In the first era ~5,000,000 are auctioned and half every era after!
 
