9fba14ede574741d05ba47bd06d6bf168c6f0b6e
CrazyCaptian <dottage@gmail.com>
2021-12-20 22:32:14 -0600

    Update chainlinkz.sol

--- whoa/chainlinkz.sol
+++ whoa/chainlinkz.sol
@@ -44,85 +44,8 @@ contract APIConsumer is ChainlinkClient {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         
         // Set the URL to perform the GET request on
-        request.add("get", "https://api.etherscan.io/api?module=proxy&action=eth_call&to=0xB6eD7644C69416d67B522e20bC294A9a9B405B31&data=4ef37628&tag=latest&apikey=YourApiKeyToken");
-        
-        // Set the path to find the desired data in the API response, where the response format is:
-        // {"RAW":
-        //   {"ETH":
-        //    {"USD":
-        //     {
-        //      "VOLUME24HOUR": xxx.xxx,
-        //     }
-        //    }
-        //   }
-        //  }
-        request.add("path", "result");
-        
-        // Multiply the result by 1000000000000000000 to remove decimals
-        int timesAmount = 10**18;
-        request.addInt("times", timesAmount);
-        
-        // Sends the request
-        return sendChainlinkRequestTo(oracle, request, fee);
-    }
-    
-    /**
-     * Receive the response in the form of uint256
-     */ 
-    function fulfill(bytes32 _requestId, bytes32 _volume) public recordChainlinkFulfillment(_requestId)
-    {
-        volume = _volume;
-    }
-
-    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
-}// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
-
-/**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
-
-/**
- * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
- * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
- */
-contract APIConsumer is ChainlinkClient {
-    using Chainlink for Chainlink.Request;
-  
-    bytes32 public volume;
-    
-    address private oracle;
-    bytes32 private jobId;
-    uint256 private fee;
-    
-    /**
-     * Network: Kovan
-     * Oracle: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8 (Chainlink Devrel   
-     * Node)
-     * Job ID: d5270d1c311941d0b08bead21fea7747
-     * Fee: 0.1 LINK
-     */
-    constructor() {
-        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
-        oracle = 0x0bDDCD124709aCBf9BB3F824EbC61C87019888bb;
-        jobId = "c6a006e4f4844754a6524445acde84a0";
-        fee = 0.01 * 10 ** 18; // (Varies by network and job)
-    }
-    
-    /**
-     * Create a Chainlink request to retrieve API response, find the target
-     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
-     */
-    function requestVolumeData() public returns (bytes32 requestId) 
-    {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        
-        // Set the URL to perform the GET request on
-        request.add("get", "https://api.etherscan.io/api?module=proxy&action=eth_call&to=0xB6eD7644C69416d67B522e20bC294A9a9B405B31&data=4ef37628&tag=latest&apikey=YourApiKeyToken");
-        
+      request.add("get", "https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=13846352&toBlock=13846352&address=0xb6ed7644c69416d67b522e20bc294a9a9b405b31&topic0=0xcf6fbb9dcea7d07263ab4f5c3a92f53af33dffc421d9d121e1c74b307e68189d&apikey=YourApiKeyToken");
+          
         // Set the path to find the desired data in the API response, where the response format is:
         // {"RAW":
         //   {"ETH":
